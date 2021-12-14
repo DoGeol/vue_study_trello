49fcef868b483be11f8ccf02a7682adc9f896b21
편도걸 <pdg2491@naver.com>
2021-12-14 20:05:46 +0900

    update - col 컴포넌트
    - col style 추가

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,20 @@
 <template>
   <section class="container">
     <t-row tagName="span">
-      <t-col tagName="span" col="12"> 1234 </t-col>
+      <t-col tagName="span" col="12" :style="{ backgroundColor: 'red' }">
+        <t-row>
+          <t-col tagName="span" col="18" :style="{ backgroundColor: 'red' }">1</t-col>
+          <t-col tagName="span" col="6" :style="{ backgroundColor: 'red' }">1</t-col>
+        </t-row>
+        <t-row>
+          <t-col tagName="span" col="4" :style="{ backgroundColor: 'blue' }">2</t-col>
+          <t-col tagName="span" col="6" :style="{ backgroundColor: 'red' }">3</t-col>
+          <t-col tagName="span" col="6" :style="{ backgroundColor: 'blue' }">4</t-col>
+          <t-col tagName="span" col="4" :style="{ backgroundColor: 'red' }">5</t-col>
+          <t-col tagName="span" col="4" :style="{ backgroundColor: 'blue' }">6</t-col>
+        </t-row>
+      </t-col>
+      <t-col tagName="span" col="12" :style="{ backgroundColor: 'gray' }"> 1234 </t-col>
     </t-row>
   </section>
 </template>
