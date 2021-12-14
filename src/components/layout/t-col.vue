7b971d811dd0887746847aecb336e17940253309
편도걸 <pdg2491@naver.com>
2021-12-14 21:18:37 +0900

    update - col 컴포넌트 responsive 추가

--- src/components/layout/t-col.vue
+++ src/components/layout/t-col.vue
@@ -6,13 +6,29 @@ export default {
       type: String,
       default: 'div',
     },
-    col: {
+    cols: {
       type: [String, Number],
       default: '24',
     },
+    isResponse: {
+      type: Boolean,
+      default: false,
+    },
   },
   render(h) {
-    const classList = [`col-${this.col}`]
+    const classList = [`col-${this.cols}`]
+    if (this.isResponse) {
+      const breakPointList = ['xs', 'md', 'lg']
+      breakPointList.forEach((breakPoint) => {
+        if (breakPoint === 'xs') {
+          classList.push(`col-${breakPoint}-${this.cols}`)
+        } else if (breakPoint === 'md') {
+          classList.push(`col-${breakPoint}-${this.cols / 2}`)
+        } else {
+          classList.push(`col-${breakPoint}-${this.cols / 4}`)
+        }
+      })
+    }
     return h(
       this.tagName,
       {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,28 @@
 <template>
   <section class="container">
-    <t-row tagName="span">
-      <t-col tagName="span" col="12" :style="{ backgroundColor: 'red' }">
-        <t-row>
-          <t-col tagName="span" col="18" :style="{ backgroundColor: 'red' }">1</t-col>
-          <t-col tagName="span" col="6" :style="{ backgroundColor: 'red' }">1</t-col>
-        </t-row>
-        <t-row>
-          <t-col tagName="span" col="4" :style="{ backgroundColor: 'blue' }">2</t-col>
-          <t-col tagName="span" col="6" :style="{ backgroundColor: 'red' }">3</t-col>
-          <t-col tagName="span" col="6" :style="{ backgroundColor: 'blue' }">4</t-col>
-          <t-col tagName="span" col="4" :style="{ backgroundColor: 'red' }">5</t-col>
-          <t-col tagName="span" col="4" :style="{ backgroundColor: 'blue' }">6</t-col>
+    <t-row>
+      <t-col cols="12">
+        <t-row :style="{ backgroundColor: 'red' }">
+          <t-col cols="6" :style="{ backgroundColor: 'gray' }">
+            <div>test1</div>
+          </t-col>
+          <t-col cols="6" :style="{ backgroundColor: 'blue' }">
+            <div>test2</div>
+          </t-col>
+          <t-col cols="6" :style="{ backgroundColor: 'yellow' }">
+            <div>test3</div>
+          </t-col>
+          <t-col cols="6" :style="{ backgroundColor: 'green' }">
+            <div>test4</div>
+          </t-col>
         </t-row>
       </t-col>
-      <t-col tagName="span" col="12" :style="{ backgroundColor: 'gray' }"> 1234 </t-col>
+      <t-col cols="6">
+        <div>test</div>
+      </t-col>
+      <t-col cols="4">
+        <div>test</div>
+      </t-col>
     </t-row>
   </section>
 </template>
