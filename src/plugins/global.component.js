5028c0d4c7305b399b7d30712b3a0a9ff7ff91cf
편도걸 <pdg2491@naver.com>
2021-12-14 21:29:51 +0900

    update - global 플러그인

--- src/plugins/global.component.js
+++ src/plugins/global.component.js
@@ -5,7 +5,7 @@ const componentFiles = require.context('@/components', true, /\.vue$/)
 const componentList = componentFiles.keys().reduce((components, path) => {
   const component = componentFiles(path)
   components.push(component.default)
-  return [...components, component.default]
+  return components
 }, [])
 
 componentList.forEach((component) => {
