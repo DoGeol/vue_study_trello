ef36b036da23446f71c7b24829d9c292ba6b644d
편도걸 <pdg2491@naver.com>
2021-12-20 18:13:01 +0900

    insert - elements > button

---
+++ src/components/elements/button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" :class="['button', size]" @click="handleClick">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TButton',
+  props: {
+    size: {
+      type: String,
+      default: 'large',
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    },
+  },
+}
+</script>
