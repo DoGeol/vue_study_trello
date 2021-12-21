bf992deb84fcfb27ed62f143302041dac6abba51
편도걸 <pdg2491@naver.com>
2021-12-21 11:27:35 +0900

    init - custom dialog
    - study용 다이얼로그 만들어보기 초기 파일 셋팅

---
+++ src/components/customDialog/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- TODO.이후 dialog 직접 만들어보기 -->
+  <transition name="transition-fade">
+    <div class="dialog-wrapper" v-if="visible">
+      <div :style="[{ width: width }, { height: height }, { minWidth: minWidth }, { minHeight: minHeight }, { maxWidth: maxWidth }, { maxHeight: maxHeight }]" class="dialog">
+        <slot />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'TCustomDialog',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    width: {
+      type: String,
+      default: '400px',
+    },
+    height: {
+      type: String,
+      default: 'auto',
+    },
+    minWidth: {
+      type: String,
+      default: '70%',
+    },
+    minHeight: {
+      type: String,
+      default: '500px',
+    },
+    maxWidth: {
+      type: String,
+      default: '100%',
+    },
+    maxHeight: {
+      type: String,
+      default: '90%',
+    },
+  },
+}
+</script>
