cb7acf677c2dbd97de6d810628b0e50d79f9d1b1
편도걸 <pdg2491@naver.com>
2021-12-21 18:18:24 +0900

    update - api crud 추가

--- src/apis/api/trello.js
+++ src/apis/api/trello.js
@@ -1,16 +1,41 @@
 import request from '../request'
 
+/* 전체 트랠로 리스트 조회*/
 export const getTrelloList = (params) => {
   return request({
-    url: `/lists?_embed=listItems`,
+    url: `/lists?_embed=tasks`,
     method: 'get',
     params,
   })
 }
 
-export const getTrelloListItem = (id) => {
+export const postTrelloList = (data) => {
   return request({
-    url: `/listItems/${id}?_embed=comments`,
+    url: `/lists`,
+    method: 'post',
+    data,
+  })
+}
+
+export const putTrelloList = (id, data) => {
+  return request({
+    url: `/lists/${id}`,
+    method: 'put',
+    data,
+  })
+}
+
+export const deleteTrelloList = (id) => {
+  return request({
+    url: `/lists/${id}`,
+    method: 'delete',
+  })
+}
+
+/* 트랠로 테스크 디테일 리스트 조회*/
+export const getTrelloTask = (id) => {
+  return request({
+    url: `/tasks/${id}?_embed=comments`,
     method: 'get',
   })
 }
