9efad96daae542deab4ef72e0e0129f3873e0d67
편도걸 <pdg2491@naver.com>
2022-01-02 23:18:44 +0900

    update - trello list 구조 변경

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,41 +16,57 @@
         </t-row>
       </template>
     </t-header>
-    <t-trello :list="trelloLists" @actionCallback="getList">
-      <template #header-title="data">
-        <h2 class="trello-list__title">{{ data.title }}</h2>
-      </template>
-      <template #tasks="tasks">
-        <template v-for="task in tasks.task">
-          <t-trello-item-default :task="task" :key="task.id" @click="handleOpenTaskDetailDialog(task)" @handleDelete="getList" />
-        </template>
-      </template>
-    </t-trello>
+    <t-row class="trello">
+      <t-draggable :list="trelloLists" group="lists" ghost-class="trello-list__wrap__ghost" filter=".button">
+        <t-col v-for="list in trelloLists" :key="list.id" class="trello-list__wrap" cols="24" lg="6" md="12" xs="24">
+          <t-trello-list :task-list="list.tasks">
+            <template #header>
+              <t-row class="mg-b-16">
+                <t-col cols="16">
+                  <h2 class="trello-list__title">{{ list.title }}</h2>
+                </t-col>
+                <t-col cols="8">
+                  <div class="flex x-end y-center">
+                    <t-button size="small" @click="handleOpenAddTaskDialog(list)">Task 추가</t-button>
+                    <t-button class="mg-l-8" size="small" @click="handleRemoveList(list)">삭제</t-button>
+                  </div>
+                </t-col>
+              </t-row>
+            </template>
+            <template #tasks="tasks">
+              <template v-for="task in tasks.task">
+                <t-trello-item-default :task="task" :key="task.id" @click="handleOpenTaskDetailDialog(task)" @handleDelete="getList" />
+              </template>
+            </template>
+          </t-trello-list>
+        </t-col>
+      </t-draggable>
+    </t-row>
 
     <t-trello-add-list-dialog :visible="addListDialog.visible" @handleClose="handleCloseAddListDialog"></t-trello-add-list-dialog>>
+    <t-trello-add-task-dialog :visible="addTaskDialog.visible" :list="addTaskDialog.list" @handleClose="handleCloseAddTaskDialog" />
     <t-trello-task-detail-dialog :visible="taskDetailDialog.visible" :task="taskDetailDialog.task" @handleClose="handleCloseTaskDetailDialog" />
   </section>
 </template>
 <script>
-import { getTrelloList } from '@/apis/api/trello'
-import TTrello from '@/components/trello/list'
-import TTrelloItemDefault from '@/components/trello/item'
+import { deleteTrelloList, getTrelloList } from '@/apis/api/trello'
 import TTrelloAddListDialog from '@/components/trello/addListDialog'
+import TTrelloAddTaskDialog from '@/components/trello/addTaskDialog'
 import TTrelloTaskDetailDialog from '@/components/trello/taskDetailDialog'
+import TTrelloList from '@/components/trello/list'
+import TTrelloItemDefault from '@/components/trello/item'
 
 export default {
   name: 'MainContainer',
   components: {
-    TTrello,
-    TTrelloItemDefault,
-    TTrelloTaskDetailDialog,
+    TTrelloAddTaskDialog,
     TTrelloAddListDialog,
+    TTrelloTaskDetailDialog,
+    TTrelloList,
+    TTrelloItemDefault,
   },
   data() {
     return {
-      addListDialog: {
-        visible: false,
-      },
       dragOptions: {
         animation: 10,
         group: '',
@@ -58,9 +74,16 @@ export default {
         ghostClass: 'trello-list__tasks-item__ghost',
       },
       trelloLists: [],
+      addListDialog: {
+        visible: false,
+      },
       taskDetailDialog: {
         visible: false,
         task: {},
+      },
+      addTaskDialog: {
+        visible: false,
+        list: {},
       },
     }
   },
@@ -98,6 +121,29 @@ export default {
       this.taskDetailDialog.visible = false
       if (reset) {
         this.getList()
+      }
+    },
+    handleOpenAddTaskDialog(list) {
+      this.addTaskDialog.list = list
+      this.addTaskDialog.visible = true
+    },
+    handleCloseAddTaskDialog(reset) {
+      this.addTaskDialog.visible = false
+      if (reset) {
+        this.getList()
+      }
+    },
+    handleRemoveList(item) {
+      console.log('handleRemoveList ::: ', item)
+      if (confirm('정말 삭제하시겠습니까?')) {
+        deleteTrelloList(item.id)
+          .then(() => {
+            alert('리스트 삭제가 완료되었습니다.')
+            this.getList()
+          })
+          .catch(() => {
+            alert('리스트 삭제가 취소되었습니다.')
+          })
       }
     },
   },
